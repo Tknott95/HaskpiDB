bba2feba76aa4076053d4228b41dff0fef5818e0
tknott95 <tknott95@hotmail.com>
2022-12-29 20:40:19 -0700

    Colors.hs - etc

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,21 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+import Database.PostgreSQL.Simple
+import Colors
+
+localPG :: ConnectInfo
+localPG = defaultConnectInfo
+        { connectHost = "127.0.0.1"
+        , connectDatabase = "testnet"
+        , connectUser = "x4"
+        , connectPassword = "lorem_ipsum"
+        }
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  putStrLn $ bCyan
+    ++ "\n CONNECTING TO: The cardano-db-sync postgresql database... \n" 
+    ++ clr
+  conn <- connect localPG
+  mapM_ print =<< (query_ conn "SELECT 1 + 1" :: IO [Only Int])
---
+++ app/libs/Colors.hs
@@ -0,0 +1,11 @@
+module Colors where
+
+dYlw  = "\ESC[2;1;33m"
+bYlw  = "\ESC[1;33m"
+bCyan = "\ESC[0;1;36m"
+bRed     = "\ESC[0;1;31m"
+
+alt    = "\ESC[38;5;65m"
+alt2    = "\ESC[38;5;69m"
+
+clr    = "\ESC[0m"
