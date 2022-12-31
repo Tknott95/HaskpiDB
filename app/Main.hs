df17d069886820473df608afc5dea8f9571fa6bf
tknott95 <tknott95@hotmail.com>
2022-12-30 19:58:09 -0700

    modular function for grabbing meta

--- app/Main.hs
+++ app/Main.hs
@@ -3,23 +3,27 @@ module Main where
 
 import Database.PostgreSQL.Simple
 import Colors
+import Text.JSON
+import qualified Data.ByteString.Lazy.Char8 as BLC
+
+data IMetadata02 = IMetadata02
+  { id    :: String
+  , name  :: String
+  , image :: String
+  , description :: String
+  } deriving (Show, Eq)
 
 localPG :: ConnectInfo
 localPG = defaultConnectInfo
-        { connectHost = "127.0.0.1"
-        , connectDatabase = "testnet"
-        , connectUser = "x4"
-        , connectPassword = "lorem_ipsum"
-        }
+  { connectHost = "127.0.0.1"
+  , connectDatabase = "testnet"
+  , connectUser = "x4"
+  , connectPassword = "lorem_ipsum"
+  }
 
-main :: IO ()
-main = do
-  putStrLn $ bCyan
-    ++ "\n CONNECTING TO: The cardano-db-sync postgresql database... \n" 
-    ++ clr
-  conn <- connect localPG
-  -- mapM_ print =<< (query_ conn "SELECT 1 + 1" :: IO [Only Int])
-  mapM_ print =<< (query_ conn "SELECT tx_metadata.json \
+
+retrieveMeta :: Connection -> String -> IO [Only String]
+retrieveMeta conn pid = query conn "SELECT tx_metadata.json \
    \ FROM ( SELECT multi_asset.id, encode(multi_asset.policy, 'hex') \
    \ AS policy_id, encode(multi_asset.name, 'escape') \
    \ AS asset_name, multi_asset.fingerprint \
@@ -27,8 +31,27 @@ main = do
    \ AS mint_or_burn_count, max(tx_metadata.id) AS tx_metadata_id, min(tx.id) \
    \ AS tx_id FROM multi_asset JOIN ma_tx_mint ON ma_tx_mint.ident = multi_asset.id JOIN tx ON tx.id = ma_tx_mint.tx_id JOIN tx_metadata ON tx_metadata.tx_id = ma_tx_mint.tx_id \
    \ WHERE tx_metadata.key IN(721) \
-   \ AND multi_asset.policy = '\\xf8ff8eb4ac1fb039ab105fcc4420217ca3792ed1f8eba8458ac3a6d6' \
-   \ GROUP BY multi_asset.id) a JOIN tx_metadata ON tx_metadata.id = a.tx_metadata_id;")
+   \ AND multi_asset.policy = ? \
+   \ GROUP BY multi_asset.id) a JOIN tx_metadata ON tx_metadata.id = a.tx_metadata_id;"  $ (Only pid)
+
+main :: IO ()
+main = do
+  putStrLn $ bCyan
+    ++ "\n CONNECTING TO: The cardano-db-sync postgresql database... \n" 
+    ++ clr
+  conn <- connect localPG
+  mapM_ print =<< retrieveMeta conn "\\xf8ff8eb4ac1fb039ab105fcc4420217ca3792ed1f8eba8458ac3a6d6"
+  -- mapM_ print =<< (query_ conn "SELECT 1 + 1" :: IO [Only Int])
+  -- mapM_ print =<< (query_ conn "SELECT tx_metadata.json \
+  --  \ FROM ( SELECT multi_asset.id, encode(multi_asset.policy, 'hex') \
+  --  \ AS policy_id, encode(multi_asset.name, 'escape') \
+  --  \ AS asset_name, multi_asset.fingerprint \
+  --  \ AS fingerprint, sum(ma_tx_mint.quantity) AS quantity, count(DISTINCT ma_tx_mint.id) \
+  --  \ AS mint_or_burn_count, max(tx_metadata.id) AS tx_metadata_id, min(tx.id) \
+  --  \ AS tx_id FROM multi_asset JOIN ma_tx_mint ON ma_tx_mint.ident = multi_asset.id JOIN tx ON tx.id = ma_tx_mint.tx_id JOIN tx_metadata ON tx_metadata.tx_id = ma_tx_mint.tx_id \
+  --  \ WHERE tx_metadata.key IN(721) \
+  --  \ AND multi_asset.policy = '\\xf8ff8eb4ac1fb039ab105fcc4420217ca3792ed1f8eba8458ac3a6d6' \
+  --  \ GROUP BY multi_asset.id) a JOIN tx_metadata ON tx_metadata.id = a.tx_metadata_id;" ::  IO [Only BLC.ByteString])
   
   -- ijk <- "SELECT tx_metadata.json \
   --  \ FROM ( SELECT multi_asset.id, encode(multi_asset.policy, 'hex') \
