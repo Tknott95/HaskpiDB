ae490e45b5ea3ae944e422de7d92b6480ee4566c
tknott95 <tknott95@hotmail.com>
2023-01-01 00:26:00 -0700

    aeson decoding bs

--- app/Main.hs
+++ app/Main.hs
@@ -196,7 +196,7 @@ main = do
   putStrLn $ show iij
 
   let abcc = A.decode iij :: Maybe Object
-  let abc = A.decode iij :: Maybe Metadata
+  let abc = A.decode iij :: Maybe IMetadata
   print $ show $ Just abc
   print $ show $ abc
   -- let z =  (i !! 0)
