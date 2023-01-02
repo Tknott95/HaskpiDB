c3a13e8cfcc2752d49626ee327e033107c6f1b31
tknott95 <tknott95@hotmail.com>
2023-01-02 15:35:20 -0700

    decoding into custom type | WORKING

--- app/Main.hs
+++ app/Main.hs
@@ -184,18 +184,6 @@ grabMeta conn pid = do
 
   return ijk
 
--- someJSON :: Value
--- someJSON = [aesonQQ|
---   {
---     "f8ff8eb4ac1fb039ab105fcc4420217ca3792ed1f8eba8458ac3a6d6": {
---       "TheCypherBox": {
---         "id": "",
---         "name: "",
---       }
---     }
---   }
--- |]
-
 
 main :: IO ()
 main = do
