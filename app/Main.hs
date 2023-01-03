081a1e3c88ce8be68a467caeb1466b88eaee77c5
tknott95 <tknott95@hotmail.com>
2023-01-03 16:11:00 -0700

    two_apis QUERY PARAM WORKING| (..).hs Servant

--- app/Main.hs
+++ app/Main.hs
@@ -30,7 +30,7 @@ import Data.Text (Text, unpack)
 policyIDStatic       = "\\xf8ff8eb4ac1fb039ab105fcc4420217ca3792ed1f8eba8458ac3a6d6" :: String
 assetNameHashStatic  = "\\x546865437970686572426f78" :: String
 
-data IGlobal = IGlobal {  polID :: Key,
+data IGlobalState = IGlobalState {  polID :: Key,
    assNameHash :: Key
 }
 
