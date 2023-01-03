175c3927f58907dc50ab1ccdd8eca26d8b850383
tknott95 <tknott95@hotmail.com>
2023-01-02 18:35:05 -0700

    DYANMIC KEYS | abstract test - PSQL.hs

--- app/libs/PSQL.hs
+++ app/libs/PSQL.hs
@@ -39,7 +39,6 @@ instance ToJSON IMetadata where
   toJSON metadataObj = object
     [
       defaultPID .= toJSON (policy_id metadataObj)
-      --(setdefaultPID "f8ff8eb4ac1fb039ab105fcc4420217ca3792ed1f8eba8458ac3a6d6") .= toJSON (policy_id metadataObj)
     ]
 
 instance ToJSON IMetadata01 where
