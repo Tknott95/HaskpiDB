dc830db63fefda8a427659e06f7ec6a0469c7bbb
Trevor Knott <Tknott95@hotmail.com>
2023-01-02 21:20:49 -0700

    ghc 9.2.5 - new BASE

--- app/Main.hs
+++ app/Main.hs
@@ -25,10 +25,10 @@ import           Servant.API
 policyIDStatic       = "\\xf8ff8eb4ac1fb039ab105fcc4420217ca3792ed1f8eba8458ac3a6d6" :: String
 assetNameHashStatic  = "\\x546865437970686572426f78" :: String
 
--- server1 ::  Connection -> Server MetaAPI_00
--- server1 conn = do
---    meta <- getMeta conn
---    return [meta]
+server1 :: [IMetadata] -> Connection -> Server MetaAPI_00
+server1 ijk conn = do
+   -- meta <- getMeta conn
+   return ijk
 
 
 getMeta :: Connection -> IO IMetadata
