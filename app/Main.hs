bec8094b3a1e3902beaae63b08eb31e41af60477
Trevor Knott <Tknott95@hotmail.com>
2023-01-02 21:26:16 -0700

    import Network.Wai pre

--- app/Main.hs
+++ app/Main.hs
@@ -21,6 +21,8 @@ import Data.ByteString.Lazy.UTF8 as BLU (fromString)
 
 import           Servant
 import           Servant.API
+import           Network.Wai
+
 
 policyIDStatic       = "\\xf8ff8eb4ac1fb039ab105fcc4420217ca3792ed1f8eba8458ac3a6d6" :: String
 assetNameHashStatic  = "\\x546865437970686572426f78" :: String
