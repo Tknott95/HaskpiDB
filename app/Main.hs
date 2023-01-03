8e1bdb2b355b80e87624d3b26641cbc015481291
tknott95 <tknott95@hotmail.com>
2023-01-02 18:14:06 -0700

    modularizing - PSQL.hs

--- app/Main.hs
+++ app/Main.hs
@@ -17,6 +17,7 @@ import qualified Data.ByteString.Lazy as LB (ByteString)
 import Data.ByteString.Lazy.UTF8 as BLU (fromString)
 
 policyIDStatic = "\\xf8ff8eb4ac1fb039ab105fcc4420217ca3792ed1f8eba8458ac3a6d6" :: String
+assetNameHashStatic = 
 
 main :: IO ()
 main = do
