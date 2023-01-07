2c20761a49b88864264c5eb94c62c69c45323177
tknott95 <tknott95@hotmail.com>
2023-01-06 18:05:08 -0700

    WORKING | Libs/API.hs

--- app/Libs/API.hs
+++ app/Libs/API.hs
@@ -1,1 +1,105 @@
 module API where
+
+import Utils (maybeUnwrap)
+import Globals 
+import PSQL
+
+import Data.Aeson (Value, encode, decode)
+import           Servant
+import           Servant.API
+-- import           Network.Wai
+-- import           Network.Wai.Handler.Warp
+
+import Metadata (MetaAPI_00)
+
+import qualified Data.ByteString.Lazy as LB (ByteString)
+import Database.PostgreSQL.Simple (Connection, connect)
+import Data.Text (Text, unpack)
+import Control.Monad.IO.Class (liftIO)
+
+
+server1 :: Connection -> Server MetaAPI_00
+server1 conn = x :<|> y
+  where 
+    x :: Text -> Handler [Value]
+    x _pid = getMeta _pid
+    y :: Text -> Text -> Handler [Value]  
+    y _pid _hashedAssetName = (getMetaByName _pid _hashedAssetName) 
+  -- return $ liftIO $ getMeta conn 3
+-- 3 is supposed to be the val of the query apram
+
+-- server2 :: Server UserAPI2
+-- server2 = return users2
+--      :<|> return albert
+--      :<|> return isaac
+
+metaAPI :: Proxy MetaAPI_00
+metaAPI = Proxy
+
+app1 :: Connection -> Application
+app1 conn = serve metaAPI (server1 conn)
+
+getMeta :: Text -> Handler [Value]
+getMeta _policyID = do
+  -- liftIO $ putStrLn $ bRed ++
+  --   "\n\n  BEFORE\n" ++
+  --   " liftIO $ putGlobPID _policyID \
+  --   \ liftIO $ getGlobIO"
+  --   ++ clr
+  -- liftIO $ getGlobIO
+  -- liftIO $ putGlobPID (unpack _policyID)
+  -- liftIO $ putStrLn $ alt ++
+  --   "\n\n  AFTER\n" ++
+  --   " liftIO $ putGlobPID _policyID \
+  --   \ liftIO $ getGlobIO"
+  --   ++ clr
+  -- liftIO $ getGlobIO
+
+  let paramPID = "\\x" ++ (unpack _policyID)
+  liftIO $ print $ "\\x" ++ (unpack _policyID)
+  -- QUERY PARAM WORKING
+  conn <- liftIO $ connect localPG
+  jj <- liftIO $ grabMetaWithPID conn paramPID
+  let j_bstring =  encode jj :: LB.ByteString
+  let jType = decode j_bstring :: Maybe Value
+  let unwrappedObj = maybeUnwrap jType
+  return [unwrappedObj]
+
+getMetaByName :: Text -> Text -> Handler [Value]
+getMetaByName _policyID _hashedAssetName = do
+  let paramPID = "\\x" ++ (unpack _policyID)
+  let hashedAssetName = "\\x" ++ (unpack _hashedAssetName)
+  -- liftIO $ putStrLn $ dYlw ++
+  --   "\n\n  BEFORE\n" ++
+  --   " liftIO $ putGlobPID _policyID \
+  --   \ liftIO $ putAssetHash (unpack _hashedAssetName) \
+  --   \ liftIO $ getGlobIO"
+  --   ++ clr
+  -- liftIO $ getGlobAllIO
+  -- liftIO $ putAssetHash (unpack _hashedAssetName)
+  -- liftIO $ putGlobPID (unpack _policyID)
+  -- liftIO $ putStrLn $ alt2 ++
+  --   "\n\n  AFTER\n" ++
+  --   " liftIO $ putGlobPID _policyID \
+  --   \ liftIO $ putAssetHash (unpack _hashedAssetName) \
+  --   \ liftIO $ getGlobIO"
+  --   ++ clr
+  -- liftIO $ getGlobAllIO
+
+  
+  -- liftIO $ print $ "\\x" ++ (unpack _hashedAssetName)
+  -- liftIO $ print $ "\\x" ++ (unpack _policyID)
+
+  --   liftIO $ print $ unhexEither "546865437970686572426f78"
+  -- DOESNT WORK IDK WHY
+
+  -- liftIO $ print getGlobAssetHash
+  -- liftIO $ print $ unsafePerformIO $ readIORef globalAssetHash
+  -- liftIO $ print $ unhexEither $ unsafePerformIO $ readIORef globalAssetHash
+  -- QUERY PARAM WORKING
+  conn <- liftIO $ connect localPG
+  jj <- liftIO $ grabMetaWithPIDAndName conn hashedAssetName paramPID
+  let j_bstring =  encode jj :: LB.ByteString
+  let jType = decode j_bstring :: Maybe Value
+  let unwrappedObj = maybeUnwrap jType
+  return [unwrappedObj]
--- app/Main.hs
+++ app/Main.hs
@@ -11,11 +11,13 @@ import Prelude hiding (id)
 
 import Colors
 import Utils as U
+import Globals
+import API
 
 import Control.Monad.IO.Class (liftIO)
 import Control.Monad.Trans.State.Lazy (runState, evalState)
 
-import Database.PostgreSQL.Simple
+-- import Database.PostgreSQL.Simple
 
 import Data.Aeson (encode, eitherDecode, decode, Object, Key, Value)
 import qualified Data.ByteString.Lazy as LB (ByteString)
@@ -27,124 +29,24 @@ import Control.Monad.State
 
 import Data.Hex
 
+import Database.PostgreSQL.Simple (connect)
+import           Network.Wai.Handler.Warp (run)
 
-
-import           Servant
-import           Servant.API
--- import           Network.Wai
-import           Network.Wai.Handler.Warp
+-- import           Servant
+-- import           Servant.API
+-- -- import           Network.Wai
+-- import           Network.Wai.Handler.Warp
 
 import Data.Text (Text, unpack, pack)
 
 import Data.IORef
 import System.IO.Unsafe
 
-import Globals
 
 import Data.Hex
 
 policyIDStatic       = "\\xf8ff8eb4ac1fb039ab105fcc4420217ca3792ed1f8eba8458ac3a6d6" :: String
 assetNameHashStatic  = "\\x546865437970686572426f78" :: String
-
-
--- putGlob :: String -> IO ()
--- -- putGlob _  = atomicModifyIORef globalPolicyIDState  (\m -> ("this-is-a-global-state" , ())) 
--- putGlob _string  = atomicModifyIORef globalPolicyIDState  (\m -> (_string , ())) 
-
---   -- ijk <- readIORef globalPolicyIDState
---   -- print 
---   -- put $ globalPolicyIDState "globalString"
-
--- getGlob :: IO ()
--- getGlob = do
---   ijk <- readIORef globalPolicyIDState
---   print ijk
-
-server1 :: Connection -> Server MetaAPI_00
-server1 conn = x :<|> y
-  where 
-    x :: Text -> Handler [Value]
-    x _pid = getMeta _pid
-    y :: Text -> Text -> Handler [Value]  
-    y _pid _hashedAssetName = (getMetaByName _pid _hashedAssetName) 
-  -- return $ liftIO $ getMeta conn 3
--- 3 is supposed to be the val of the query apram
-
--- server2 :: Server UserAPI2
--- server2 = return users2
---      :<|> return albert
---      :<|> return isaac
-
-metaAPI :: Proxy MetaAPI_00
-metaAPI = Proxy
-
-app1 :: Connection -> Application
-app1 conn = serve metaAPI (server1 conn)
-
-getMeta :: Text -> Handler [Value]
-getMeta _policyID = do
-  -- liftIO $ putStrLn $ bRed ++
-  --   "\n\n  BEFORE\n" ++
-  --   " liftIO $ putGlobPID _policyID \
-  --   \ liftIO $ getGlobIO"
-  --   ++ clr
-  -- liftIO $ getGlobIO
-  -- liftIO $ putGlobPID (unpack _policyID)
-  -- liftIO $ putStrLn $ alt ++
-  --   "\n\n  AFTER\n" ++
-  --   " liftIO $ putGlobPID _policyID \
-  --   \ liftIO $ getGlobIO"
-  --   ++ clr
-  -- liftIO $ getGlobIO
-
-  let paramPID = "\\x" ++ (unpack _policyID)
-  liftIO $ print $ "\\x" ++ (unpack _policyID)
-  -- QUERY PARAM WORKING
-  conn <- liftIO $ connect localPG
-  jj <- liftIO $ grabMetaWithPID conn paramPID
-  let j_bstring =  encode jj :: LB.ByteString
-  let jType = decode j_bstring :: Maybe Value
-  let unwrappedObj = maybeUnwrap jType
-  return [unwrappedObj]
-
-getMetaByName :: Text -> Text -> Handler [Value]
-getMetaByName _policyID _hashedAssetName = do
-  let paramPID = "\\x" ++ (unpack _policyID)
-  let hashedAssetName = "\\x" ++ (unpack _hashedAssetName)
-  -- liftIO $ putStrLn $ dYlw ++
-  --   "\n\n  BEFORE\n" ++
-  --   " liftIO $ putGlobPID _policyID \
-  --   \ liftIO $ putAssetHash (unpack _hashedAssetName) \
-  --   \ liftIO $ getGlobIO"
-  --   ++ clr
-  -- liftIO $ getGlobAllIO
-  -- liftIO $ putAssetHash (unpack _hashedAssetName)
-  -- liftIO $ putGlobPID (unpack _policyID)
-  -- liftIO $ putStrLn $ alt2 ++
-  --   "\n\n  AFTER\n" ++
-  --   " liftIO $ putGlobPID _policyID \
-  --   \ liftIO $ putAssetHash (unpack _hashedAssetName) \
-  --   \ liftIO $ getGlobIO"
-  --   ++ clr
-  -- liftIO $ getGlobAllIO
-
-  
-  -- liftIO $ print $ "\\x" ++ (unpack _hashedAssetName)
-  -- liftIO $ print $ "\\x" ++ (unpack _policyID)
-
-  --   liftIO $ print $ unhexEither "546865437970686572426f78"
-  -- DOESNT WORK IDK WHY
-
-  -- liftIO $ print getGlobAssetHash
-  -- liftIO $ print $ unsafePerformIO $ readIORef globalAssetHash
-  -- liftIO $ print $ unhexEither $ unsafePerformIO $ readIORef globalAssetHash
-  -- QUERY PARAM WORKING
-  conn <- liftIO $ connect localPG
-  jj <- liftIO $ grabMetaWithPIDAndName conn hashedAssetName paramPID
-  let j_bstring =  encode jj :: LB.ByteString
-  let jType = decode j_bstring :: Maybe Value
-  let unwrappedObj = maybeUnwrap jType
-  return [unwrappedObj]
 
 main :: IO ()
 main = do
