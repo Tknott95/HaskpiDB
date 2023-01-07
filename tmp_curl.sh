924a31e64e087146ed802529afb16a4d7d6c126c
Trevor Knott <Tknott95@hotmail.com>
2023-01-06 19:47:07 -0700

    tmp_curl.sh

--- tmp_curl.sh
+++ tmp_curl.sh
@@ -3,8 +3,9 @@
 url=127.0.0.1
 port=8081
 
+tmp_pol_id=f8ff8eb4ac1fb039ab105fcc4420217ca3792ed1f8eba8458ac3a6d6
+tmp_name_hash=546865437970686572426f78 
 
-curl http://$url:$port/metadata/f8ff8eb4ac1fb039ab105fcc4420217ca3792ed1f8eba8458ac3a6d6
-echo -e "\n"
-curl http://$url:$port/metadata_by_name/f8ff8eb4ac1fb039ab105fcc4420217ca3792ed1f8eba8458ac3a6d6/546865437970686572426f78 
+curl http://$url:$port/metadata/$tmp_pol_id ; echo -e "\n"
+curl http://$url:$port/metadata_by_name/$tmp_pol_id/$tmp_name_hash
 
