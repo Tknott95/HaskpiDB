f529964a2dde5b7d8c9c767d42422bc8115513cb
Trevor Knott <Tknott95@hotmail.com>
2023-01-06 19:44:08 -0700

    tmp_curl.sh

--- tmp_curl.sh
+++ tmp_curl.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-curl http://localhost:8081/metadata/f8ff8eb4ac1fb039ab105fcc4420217ca3792ed1f8eba8458ac3a6d6
+url=127.0.0.1
+port=8081
+
+
+curl http://$url:$port/metadata/f8ff8eb4ac1fb039ab105fcc4420217ca3792ed1f8eba8458ac3a6d6
 echo -e "\n"
-curl http://localhost:8081/metadata_by_name/f8ff8eb4ac1fb039ab105fcc4420217ca3792ed1f8eba8458ac3a6d6/546865437970686572426f78 
+curl http://$url:$port/metadata_by_name/f8ff8eb4ac1fb039ab105fcc4420217ca3792ed1f8eba8458ac3a6d6/546865437970686572426f78 
 
