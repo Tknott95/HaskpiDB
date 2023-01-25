e0c62cfe699ed00c5445192111590000d1077d91
tknott95 <tknott95@hotmail.com>
2023-01-25 08:01:08 -0700

    crashed - fix metaByStake

--- app/Libs/PSQL.hs
+++ app/Libs/PSQL.hs
@@ -63,10 +63,10 @@ grabMetaWithPIDAndName conn asName pid = do
 
 -- stake_test1uz87hafc2aqhhfrvarqtxf3c25lzhfqa938l8pl4t9fu9jqj0jamq
 grabMetaWithStakeKey :: Connection -> String -> IO AT.Value
-grabMetaWithStakeKey conn sKey = do
+grabMetaWithStakeKey conn skey = do
   [Only ijk] <- query conn "SELECT json FROM utxo_view \
   \ JOIN stake_address ON stake_address.id = utxo_view.stake_address_id \
   \ RIGHT JOIN tx_metadata \
   \ ON utxo_view.tx_id=tx_metadata.tx_id \
-  \ WHERE view = ?;" [sKey :: String]
+  \ WHERE view = ?';" [skey :: String]
   return ijk
