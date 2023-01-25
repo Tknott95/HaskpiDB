9fd6ada626b76637f25da58d3d85fdd463fee6bc
tknott95 <tknott95@hotmail.com>
2023-01-25 08:09:52 -0700

    crashed from left string char - PSQL.hs

--- tmp_curl.sh
+++ tmp_curl.sh
@@ -5,7 +5,7 @@ port=1339
 
 tmp_pol_id=f8ff8eb4ac1fb039ab105fcc4420217ca3792ed1f8eba8458ac3a6d6
 tmp_name_hash=546865437970686572426f78 
-skey=stake_test1uz87hafc2aqhhfrvarqtxf3c25lzhfqa938l8pl4t9fu9jqj0jamq
+skey='stake_test1uz87hafc2aqhhfrvarqtxf3c25lzhfqa938l8pl4t9fu9jqj0jamq'
 
 curl http://$url:$port/metadata/$tmp_pol_id ; echo -e "\n"
 curl http://$url:$port/metadata_by_name/$tmp_pol_id/$tmp_name_hash;  echo -e "\n"
